d7acee0466703cb33c49e310921a82e9412e5f4b
Matthew Butt <matthew@matthewbutt.com>
2012-06-14 15:50:46 +0100

    Got Bill passing

--- displayNetwork.js
+++ displayNetwork.js
@@ -1,11 +1,15 @@
 var fs = require('fs')
-,	xml2json = require('./xml2json')
 ,	stubJson;
 
 stubJson = {"Network":{"Programmer":[{"name":"Bill","Recommendations":{"Recommendation":["Jason","Jill","Nick","Stu"]},"Skills":{"Skill":["Ruby","Perl","PHP"]}},{"name":"Dave","Recommendations":{"Recommendation":"Jill"},"Skills":{"Skill":["Java","C#"]}},{"name":"Ed","Recommendations":{"Recommendation":["Liz","Rick"]},"Skills":{"Skill":["C++","Python"]}},{"name":"Frank","Recommendations":{"Recommendation":"Nick"},"Skills":{"Skill":["Perl","Ruby","C++"]}},{"name":"Jason","Recommendations":{"Recommendation":["Dave","Liz"]},"Skills":{"Skill":["Java","Ruby"]}},{"name":"Jill","Recommendations":{"Recommendation":"Nick"},"Skills":{"Skill":["C++","Ruby"]}},{"name":"Liz","Recommendations":{"Recommendation":"Bill"},"Skills":{"Skill":["C#","C++","Java"]}},{"name":"Nick","Recommendations":{"Recommendation":"Stu"},"Skills":{"Skill":["C#","Java"]}},{"name":"Rick","Recommendations":{"Recommendation":"Ed"},"Skills":{"Skill":["Java","PHP"]}},{"name":"Stu","Recommendations":{"Recommendation":"Frank"},"Skills":{"Skill":["C++","Perl"]}}]}};
 
 function display(){
-	return "Programmers\tSkills\tRecommends";
+	var output = "Programmers\tSkills\tRecommends\n"
+	,	programmer = stubJson.Network.Programmer[0];
+	
+	output += programmer.name + "\t" + programmer.Skills.Skill.join(", ") + "\t" + programmer.Recommendations.Recommendation.join(", ");
+	
+	return output;
 }  
 
 console.log(display());
