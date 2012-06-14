3894cc809f3fb02122f8af8658d7537cceebde50
Michael Lam <michael@michaellam.co.uk>
2012-06-14 16:06:51 +0100

    read from the real xml file

--- displayNetwork.js
+++ displayNetwork.js
@@ -1,7 +1,8 @@
 var fs = require('fs')
-,	stubJson;
+,	xml2json = require('./xml2json')
+,	json;
 
-stubJson = {"Network":{"Programmer":[{"name":"Bill","Recommendations":{"Recommendation":["Jason","Jill","Nick","Stu"]},"Skills":{"Skill":["Ruby","Perl","PHP"]}},{"name":"Dave","Recommendations":{"Recommendation":"Jill"},"Skills":{"Skill":["Java","C#"]}},{"name":"Ed","Recommendations":{"Recommendation":["Liz","Rick"]},"Skills":{"Skill":["C++","Python"]}},{"name":"Frank","Recommendations":{"Recommendation":"Nick"},"Skills":{"Skill":["Perl","Ruby","C++"]}},{"name":"Jason","Recommendations":{"Recommendation":["Dave","Liz"]},"Skills":{"Skill":["Java","Ruby"]}},{"name":"Jill","Recommendations":{"Recommendation":"Nick"},"Skills":{"Skill":["C++","Ruby"]}},{"name":"Liz","Recommendations":{"Recommendation":"Bill"},"Skills":{"Skill":["C#","C++","Java"]}},{"name":"Nick","Recommendations":{"Recommendation":"Stu"},"Skills":{"Skill":["C#","Java"]}},{"name":"Rick","Recommendations":{"Recommendation":"Ed"},"Skills":{"Skill":["Java","PHP"]}},{"name":"Stu","Recommendations":{"Recommendation":"Frank"},"Skills":{"Skill":["C++","Perl"]}}]}};
+json = JSON.parse(xml2json.parser(fs.readFileSync("ProNet.xml", 'utf8')));
 
 function maybeArrayToString(input) {
 	if(input.join) return input.join(", ");
@@ -10,17 +11,23 @@ function maybeArrayToString(input) {
 
 function display(){
 	var output = "Programmers\tSkills\tRecommends"
-	,	programmerCount = stubJson.Network.Programmer.length
+	,	programmerCount = json.Network.Programmer.length
 	,	i
 	,	programmer;
 	
 	for(i = 0; i < programmerCount; i = i + 1){
-		programmer = stubJson.Network.Programmer[i];
+		programmer = json.Network.Programmer[i];
 		output += "\n" + programmer.name + "\t" + maybeArrayToString(programmer.Skills.Skill) + "\t" + maybeArrayToString(programmer.Recommendations.Recommendation);
 	}
 		
 	return output;
 }  
+
+function readXml(){
+	
+	return callback((file));
+}
+
 
 console.log(display());
 
