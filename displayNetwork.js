a76a120dca176acd0b15e555c5683203aaf3ceb5
Matthew Butt <matthew@matthewbutt.com>
2012-06-14 15:43:20 +0100

    Started implementation of listing

--- displayNetwork.js
+++ displayNetwork.js
@@ -1,17 +1,13 @@
-var fs = require('fs')
-,	xml2json = require('./xml2json');
+var fs = require('fs');
 
 function display(){
-  	return "Programmers\tSkills\tRecommends\n"; 
+  	
+	var fileContents = fs.readFileSync("ProNet.xml", 'utf8');
+  	return "Programmers\tSkills\tRecommends";
 }
   
 console.log(display());
 
-var file = fs.readFile("ProNet.xml", 'utf8', function(err, data){
-//	console.log(data);
-	console.log(xml2json.parser(data));
-
-});
 
 
 module.exports = {
